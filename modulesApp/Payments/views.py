8f565244156e171d12a7086c92800e50d25a07cd
Nixjose <analista0093@gmail.com>
2022-09-25 15:18:22 -0400

    cambios nixon payments

--- modulesApp/Payments/views.py
+++ modulesApp/Payments/views.py
@@ -13,15 +13,71 @@ from django.core.files.storage import FileSystemStorage
 
 from datetime import datetime
 
+from ..App.models import ConfTablasConfiguracion
+
 # Create your views here.
+
+def get_metodo_pgs(request):
+
+    context={}
+    if request.method == 'POST':
+        
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            
+            if request.body:
+                
+                data= json.load(request)
+                if data['acceso'] == 'show':
+                    metodo_pgs=ConfTablasConfiguracion.obtenerHijos("method_pgs")
+                    context={'metodo_pgs':metodo_pgs}
+                    html_template= (loader.get_template('Payments/get_metodo_pgs.html'))
+
+                    return HttpResponse(html_template.render(context, request))
+
 
 def payments(request):
     
-    context={}
+    concepto_pgs=ConfTablasConfiguracion.obtenerHijos("concepto_pgs")
+    metodo_pgs=ConfTablasConfiguracion.obtenerHijos("method_pgs")
 
     html_template= (loader.get_template('Payments/payments.html'))
 
+    context={'metodo_pgs':metodo_pgs, 'concepto_pgs':concepto_pgs}
     return HttpResponse(html_template.render(context, request))
 
+
+
+def get_formPayments(request):
+    context={}
+    if request.method == 'POST':
+        
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            
+            if request.body:
+                
+                data= json.load(request)
+                if data['acceso'] == 'show':
+                    
+                    context={}
+                    html_template= (loader.get_template('Payments/formPayments.html'))
+
+                    return HttpResponse(html_template.render(context, request))
+                    
+
+def get_formPgTransf(request):
+    context={}
+    if request.method == 'POST':
+        
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            
+            if request.body:
+                
+                data= json.load(request)
+                if data['acceso'] == 'show':
+                    
+                    context={}
+                    html_template= (loader.get_template('Payments/get_form_pg_transf.html'))
+
+                    return HttpResponse(html_template.render(context, request))
 
 
